bd0f2cfb8661f6332cd0805e3b82ca2ed3694550
Miguel9669 <antoniomiguel12345677@gmail.com>
2021-08-25 00:18:06 +0100

    comecando a troca de informacoes no socket

--- server.js
+++ server.js
@@ -8,11 +8,29 @@ const socket = io(server)
 
 
 app.use(express.static('public'))
-
+let connection = []
+let names = []
 socket.on('connection', (socket)=>{
     
-    console.log('conectaram-se')
+    connection.push(socket)
+    socket.on('message', (ola)=>{
+        if(ola.includes('N:')){
+            if(names.includes(ola.substr(2))){
+                socket.send('N')
+                console.log('emiti')
+                
+            }else{
+                names.push(ola.substr(2))
+                console.log(names)
+            }
+            
+        }
+    })
+    
+    
 })
+
+
 
 server.listen(8000, ()=>{
     console.log('server on')

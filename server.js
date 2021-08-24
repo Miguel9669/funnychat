ae505f1096f483c71e55fc7abde4abc999379def
Miguel9669 <antoniomiguel12345677@gmail.com>
2021-08-24 22:28:06 +0100

    socket io inserido

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express')
 const http = require('http')
+const io = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
+const socket = io(server)
 
 
 app.use(express.static('public'))
 
+socket.on('connection', (socket)=>{
+    
+    console.log('conectaram-se')
+})
 
 server.listen(8000, ()=>{
     console.log('server on')

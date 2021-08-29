2d0c83e0341a1132d487d53f946394918c608d57
Miguel9669 <antoniomiguel12345677@gmail.com>
2021-08-29 10:10:06 +0100

    troca acontece

--- server.js
+++ server.js
@@ -14,17 +14,38 @@ socket.on('connection', (socket)=>{
     
     connection.push(socket)
     socket.on('message', (ola)=>{
+        
         if(ola.includes('N:')){
             if(names.includes(ola.substr(2))){
                 socket.send('N')
                 console.log('emiti')
+                console.log(names)
                 
             }else{
+                socket.send('S')
                 names.push(ola.substr(2))
+                
+                connection.forEach(s => s.send('s:'+ ola.substr(2)+' Join The party'))
                 console.log(names)
             }
             
         }
+        if(ola.includes('M:')){
+            let person = connection.indexOf(socket)
+            connection.forEach(s => s.send('M:'+ names[person]+': '+ ola.substr(2)))
+        }
+    })
+    socket.on('disconnect', function () {
+        console.log('xau')
+        let person1 = connection.indexOf(socket)
+        connection.forEach(s => s.send('s:'+ names[person1]+' Left The party'))
+        setTimeout(()=>{
+            connection.splice(person1, 1);
+            names.splice(person1,1)
+            console.log(names)
+        }, 1000)
+        
+        
     })
     
     
